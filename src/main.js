ded4896c649f88f990796f16988c1600d0312ee4
ajilk <ajilkhomov@gmail.com>
2020-03-28 19:13:08 -0400

    Use JavaScript antlr runtime to display tree

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const antlr = require('antlr4/index');
+const fs = require('fs');
+
+const Lexer = require('./parser/btrsLexer').btrsLexer;
+const Parser = require('./parser/btrsParser').btrsParser;
+
+const input = fs.readFileSync(__dirname+'/sample.bai');
+const chars = new antlr.InputStream(input.toString());
+const lexer = new Lexer(chars);
+const tokens = new antlr.CommonTokenStream(lexer);
+const parser = new Parser(tokens);
+const tree = parser.btrsfile();
+
+console.log(tree.toStringTree(parser.ruleNames));
