51ea6e7247e3548afc0ba2e9a0b6fa360d16d858
ajilk <ajilkhomov@gmail.com>
2020-04-11 18:01:52 -0400

    Setup dummy listener

---
+++ Makefile
@@ -0,0 +1,10 @@
+CLASSPATH=/usr/local/Cellar/antlr/4.8_1/antlr-4.8-complete.jar:.
+java:
+	antlr -Dlanguage=Java ./btrs.g4
+	javac -cp ${CLASSPATH} ./btrs*.java
+run: 
+	grun btrs btrsFile -tree ./data/01.bai
+g: 
+	grun btrs btrsFile -gui ./data/01.bai
+clean:
+	rm -rf *.{class,java,interp,tokens}
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,31 @@
 import { ANTLRInputStream as InputStream, Lexer, CommonTokenStream, Parser } from 'antlr4ts';
-import { AbstractParseTreeVisitor, ParseTreeWalker } from 'antlr4ts/tree';
+
 import fs from 'fs';
 import path from 'path';
 
 import { btrsLexer } from './lib/btrsLexer';
-import { btrsParser } from './lib/btrsParser';
-import { btrsVisitor } from './lib/btrsVisitor';
+import { btrsParser, FileHeaderContext } from './lib/btrsParser';
+import { btrsListener } from './lib/btrsListener';
+import { ParseTreeWalker, ParseTreeListener } from 'antlr4ts/tree';
 
-class BTRSFileVisitor extends AbstractParseTreeVisitor<number> implements btrsVisitor<number> {
-  defaultResult() {
-    return 0;
+
+class MyListener implements btrsListener {
+  enterFileHeader(context: FileHeaderContext) {
+    console.log(context.toStringTree());
   }
-  visitBtrsFile(ctx: any) {
-    console.log('visited btrsFile');
-    console.log(ctx);
-    return 1;
-  }
+
+  visitTerminal() { }
+  visitErrorNode() { }
+  enterEveryRule() { }
+  exitEveryRule() { }
 }
 
-export function visit() {
-  let input: Buffer = fs.readFileSync(path.resolve(__dirname, '../data/01.bai'));
-  let chars: InputStream = new InputStream(input.toString());
-  let lexer: Lexer = new btrsLexer(chars);
-  let tokens: CommonTokenStream = new CommonTokenStream(lexer);
-  let parser: btrsParser = new btrsParser(tokens);
-  let tree = parser.btrsFile();
+let input: Buffer = fs.readFileSync(path.resolve(__dirname, '../data/01.bai'));
+let chars: InputStream = new InputStream(input.toString());
+let lexer: Lexer = new btrsLexer(chars);
+let tokens: CommonTokenStream = new CommonTokenStream(lexer);
+let parser: btrsParser = new btrsParser(tokens);
+let tree = parser.btrsFile();
 
-  const btrsFileVisitor = new BTRSFileVisitor();
-  btrsFileVisitor.visit(tree);
-}
-
-visit();
+const listener: MyListener = new MyListener();
+ParseTreeWalker.DEFAULT.walk(listener, tree);
